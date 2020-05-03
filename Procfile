a25d3ba36929f5a09161935fb746202c7cec1bb9
JD2xor <flere210.0323@gmail.com>
2020-05-03 15:51:04 +0900

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ogp-parse-api
---
+++ server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	
+	"github.com/labstack/echo"
+	"github.com/otiai10/opengraph"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/ogp", getOgp)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "1323"
+	}
+	e.Logger.Fatal(e.Start(":" + port))
+}
+
+func getOgp(ctx echo.Context) error {
+	url := ctx.QueryParam("url")
+	if len(url) == 0 {
+		return ctx.String(http.StatusBadRequest, "Url is empty")
+	}
+
+	og, err := opengraph.Fetch(url)
+	if err != nil {
+		return ctx.String(http.StatusInternalServerError, fmt.Sprintf("Error: %s", err.Error()))
+	}
+	
+	og = og.ToAbsURL()
+
+	return ctx.JSON(http.StatusOK, og)	
+}
